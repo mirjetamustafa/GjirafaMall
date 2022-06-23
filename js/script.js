60afdb04a67e97b60a5eb488d354125a2ba865a4
mirjetaa <mmirjeta321@gmail.com>
2022-06-23 16:48:16 +0200

    finished landing page

---
+++ js/script.js
@@ -0,0 +1,38 @@
+//Dyqanet
+$('.owl-carousel').owlCarousel({
+    loop:false,
+    margin:10,
+    nav:true,
+    dots: false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:8
+        }
+    }
+})
+
+// End Dyqanet
+
+//Load More
+let loadMore = document.querySelector('#load-more');
+let currentItem = 5;
+
+loadMore.onclick = () => {
+    let boxes = [...document.querySelectorAll('.row .gjirafa-2 .col')];
+
+    for(var i = currentItem; i < currentItem + 5; i++){
+        boxes[i].style.display = "inline-block";
+    }
+    currentItem += 5;
+
+    if(currentItem >= boxes.length) {
+        loadMore.style.display = "none";
+    }
+}
+//End Load More
